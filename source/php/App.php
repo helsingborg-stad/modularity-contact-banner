8fbe40ab9afad104f3c9698af3d3e31f89b1a58d
Petter Andersson <petter.andersson@helsingborg.se>
2025-11-20 13:28:39 +0000

    vite migration

--- source/php/App.php
+++ source/php/App.php
@@ -2,15 +2,21 @@
 
 namespace ModularityContactBanner;
 
+use WpUtilService\Features\Enqueue\EnqueueManager;
+
 class App
 {
-    public function __construct()
-    {
+    public function __construct(
+        private EnqueueManager $wpEnqueue,
+    ) {
         add_action('init', array($this, 'registerModule'));
         add_filter('Municipio/Admin/Acf/PrefillIconChoice', array($this, 'addFieldNameToPrefillList'));
+        add_action('wp_enqueue_scripts', function () {
+            $this->wpEnqueue->add('css/modularity-contact-banner.css');
+        });
     }
 
-    public function addFieldNameToPrefillList(array $items) 
+    public function addFieldNameToPrefillList(array $items)
     {
         $items[] = 'mod_contactbanner_cta_icon';
 
@@ -24,10 +30,7 @@ class App
     public function registerModule()
     {
         if (function_exists('modularity_register_module')) {
-            modularity_register_module(
-                MODULARITYCONTACTBANNER_MODULE_PATH,
-                'ContactBanner'
-            );
+            modularity_register_module(MODULARITYCONTACTBANNER_MODULE_PATH, 'ContactBanner');
         }
     }
 }
